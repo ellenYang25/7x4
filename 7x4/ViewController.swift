b75775f38a0bebe0a904c929ac12aa99a86c0105
Yang Ellen <dogdog926@msn.com>
2018-09-02 13:54:34 +0800

    Initial Commit

---
+++ 7x4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7x4
+//
+//  Created by Ellen Yang on 2018/9/2.
+//  Copyright © 2018年 Ellen Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
